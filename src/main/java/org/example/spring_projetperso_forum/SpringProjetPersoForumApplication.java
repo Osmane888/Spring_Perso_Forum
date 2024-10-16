868c11975ba284feb7042d346cb69185a448f0a7
osmane <osmane688@hotmail.com>
2024-10-16 20:45:19 +0200

    project initialization

---
+++ src/main/java/org/example/spring_projetperso_forum/SpringProjetPersoForumApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_projetperso_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjetPersoForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjetPersoForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_projetperso_forum/SpringProjetPersoForumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_projetperso_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjetPersoForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
